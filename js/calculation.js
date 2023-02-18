9723f2d01b30333e5e1d1b8a140f15edf590740c
Nahid-subi <scientisthassannahid@gmail.com>
2023-02-18 13:03:29 +0600

    first two card calculation and validation added

--- js/calculation.js
+++ js/calculation.js
@@ -1,3 +1,45 @@
-document.getElementById('add').addEventListener('click',function(){
-    console.log('button click')
+document.getElementById('triangle-btn').addEventListener('click',function(){
+    const b = inputValueToNumber("triangle-field-1");
+    const h = inputValueToNumber("triangle-field-2")
+    const area = 0.5 * b * h;
+    if(isNaN(area) || area < 0){
+        alert("Please Provide Valid Number");
+        return;
+    }
+    displayData(area.toFixed(2))
 })
+document.getElementById('rectangle-btn').addEventListener('click',function(){
+    const w = inputValueToNumber("rectangleField-1");
+    const l = inputValueToNumber("rectangleField-2")
+    const area = w * l;
+    if(isNaN(area) || area < 0){
+        alert("Please Provide Valid Number");
+        return;
+    }
+    displayData(area.toFixed(2))
+})
+
+
+
+
+// take input value string convert to number
+function inputValueToNumber(id){
+    const value = document.getElementById(id).value ;
+    return parseFloat(value);
+}
+
+// common function to display data
+let count = 0; 
+function displayData(area){
+    count += 1;
+    const container = document.getElementById("table-container");
+    const tr = document.createElement("tr");
+    tr.innerHTML=`
+    <td>${count}</td>
+    <td>${area}<span>cm<sup>2</sup></span> </td>
+    <td><button
+    id="triangle-btn"
+    class="bg-sky-500 font-semibold hover:bg-sky-700 text-white py-2 px-6 rounded">Covert to m<sup>2</sup></button></td>
+    `;
+    container.appendChild(tr);
+}
